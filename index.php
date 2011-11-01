ba2ce5b51909e702a42bd288b69c8e04085b13f5
Bradley Few <few.bradley@gmail.com>
2011-11-01 14:41:05 +0000

    Folder architecture
    - Created Assets folder to store all Assets to the project
    - Uploaded humans.txt and robots.txt
    - Uploaded standard php file to work from

---
+++ index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<!--[if IE 7]><html class="no-js ie7" lang="en"><![endif]-->
+<!--[if IE 8]><html class="no-js ie8" lang="en"><![endif]-->
+<!--[if IE 9]><html class="no-js ie9" lang="en"><![endif]-->
+<!--[if gt IE 9]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>	
+    <title><?= (basename($_SERVER['PHP_SELF']) === 'index.php') ? 'Welcome' : ucwords(str_replace(array('.php', '-'), array('', ' '), basename($_SERVER['PHP_SELF']))); ?> | Parent Information Portal</title>
+    <script src="Assets/Scripts/modernizr.min.js"></script>
+    <meta charset="utf-8">
+    <meta name="author" content="Storm Creative" />
+    <link type="text/plain" rel="author" href="humans.txt">
+    <link rel="stylesheet" href="Assets/Styles/fonts.css" />
+    <link rel="stylesheet" href="Assets/Styles/structure.css" />
+    <link rel="stylesheet" media="screen and (-webkit-min-device-pixel-ratio:0)" href="Assets/Styles/webkit.css" />
+    <!--[if IE]>
+    	<link rel="stylesheet" href="Assets/Styles/IE.css" />
+    <![endif]-->
+</head>
+<?php flush(); ?>
+<body>	
+	// Content
+</body>
+</html>
