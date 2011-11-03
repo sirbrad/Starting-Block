77089d0a8c1c24dffabccb4bb7d9eed595742656
Bradley Few <few.bradley@gmail.com>
2011-11-03 11:11:21 +0000

    Minor commit

--- index.php
+++ index.php
@@ -7,11 +7,10 @@
     <title><?= (basename($_SERVER['PHP_SELF']) === 'index.php') ? 'Welcome' : ucwords(str_replace(array('.php', '-'), array('', ' '), basename($_SERVER['PHP_SELF']))); ?> | Site Name</title>
     <script src="Assets/Scripts/modernizr.min.js"></script>
     <meta charset="utf-8">
-    <meta name="author" content="Storm Creative" />
+    <meta name="author" content="sirBrad" />
     <link type="text/plain" rel="author" href="humans.txt">
     <link rel="stylesheet" href="Assets/Styles/fonts.css" />
     <link rel="stylesheet" href="Assets/Styles/structure.css" />
-    <link rel="stylesheet" media="screen and (-webkit-min-device-pixel-ratio:0)" href="Assets/Styles/webkit.css" />
     <!--[if IE]>
     	<link rel="stylesheet" href="Assets/Styles/IE.css" />
     <![endif]-->
