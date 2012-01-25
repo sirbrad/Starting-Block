6914374be3f9270db78e086fbcb646359f12669d
Bradley Few <few.bradley@gmail.com>
2012-01-25 09:54:06 +0000

    Updated main.js to not have loads of previous code inside.

--- Assets/Scripts/main.js
+++ Assets/Scripts/main.js
@@ -4,38 +4,4 @@ require.config({
 	}
 });
 
-require(['require'], function(require){
-	
-	if (/*check for id*/) {
-		require('login');
-	}
-	
-	if (/*check for id*/) {
-		require('datepicker');
-	}
-	
-	if (/*check for id*/) {
-		require('confirm-delete');
-	}
-	
-	if (/*check for id*/) {
-		require('document-delete');
-	}
-	
-	if (!!storm_sortable) {
-		require('sortable');
-	}
-	
-	if (!!storm_uploadify) {
-		require('uploadify');
-	}
-	
-	if (!!storm_ckeditor) {
-		require('ckeditor');
-	}
-	
-	if (!!storm_filter) {
-		require('filter');
-	}
-	
-});
+require(['module']);
